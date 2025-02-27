238a0919b9863123a9729176416e2a341d8b200e
Deniz Osmanovic <47736428+DOsmanovic@users.noreply.github.com>
2025-02-27 22:42:22 +0100

    telescope truncate filepaths

--- init.lua
+++ init.lua
@@ -393,11 +393,11 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          -- "tail" shows only the filename, not the full path
+          path_display = { 'truncate' },
+          -- i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
