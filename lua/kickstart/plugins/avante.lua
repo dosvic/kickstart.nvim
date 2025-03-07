47f86b508b87b2c96f57f66acd6de560b6393774
Deniz Osmanovic <47736428+DOsmanovic@users.noreply.github.com>
2025-03-07 21:57:30 +0100

    break up plugins into multiple files

---
+++ lua/kickstart/plugins/avante.lua
@@ -0,0 +1,74 @@
+return {
+  "yetone/avante.nvim",
+  event = "VeryLazy",
+  version = false,
+  config = function()
+    require("avante").setup {
+      provider = "copilot",
+      copilot = {
+        model = "claude-3.7-sonnet",
+        temperature = 1,
+        max_tokens = 20000,
+      },
+      vendors = {
+        ["openrouter-deepseek-r1"] = {
+          __inherited_from = "openai",
+          model = "deepseek/deepseek-r1",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+        },
+        ["openrouter-deepseek-v3"] = {
+          __inherited_from = "openai",
+          model = "deepseek/deepseek-chat",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+        },
+        ["openrouter-claude-3.7-sonnet"] = {
+          __inherited_from = "openai",
+          model = "anthropic/claude-3.7-sonnet",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+        },
+        ["openrouter-qwen/qwq-32b"] = {
+          __inherited_from = "openai",
+          model = "qwen/qwq-32b",
+          endpoint = "https://openrouter.ai/api/v1",
+          api_key_name = "OPENROUTER_API_KEY",
+        },
+      },
+      behaviour = {
+        auto_suggestions = false,
+      },
+    }
+  end,
+  build = "make",
+  dependencies = {
+    "stevearc/dressing.nvim",
+    "nvim-lua/plenary.nvim",
+    "MunifTanjim/nui.nvim",
+    "hrsh7th/nvim-cmp",
+    "nvim-tree/nvim-web-devicons",
+    "zbirenbaum/copilot.lua",
+    {
+      "HakonHarnes/img-clip.nvim",
+      event = "VeryLazy",
+      opts = {
+        default = {
+          embed_image_as_base64 = false,
+          prompt_for_file_name = false,
+          drag_and_drop = {
+            insert_mode = true,
+          },
+          use_absolute_path = true,
+        },
+      },
+    },
+    {
+      "MeanderingProgrammer/render-markdown.nvim",
+      opts = {
+        file_types = { "markdown", "Avante" },
+      },
+      ft = { "markdown", "Avante" },
+    },
+  },
+}
---
+++ lua/kickstart/plugins/init.lua
@@ -0,0 +1,17 @@
+return {
+  require("kickstart.plugins.vim-sleuth"),
+  require("kickstart.plugins.gitsigns"),
+  require("kickstart.plugins.which-key"),
+  require("kickstart.plugins.telescope"),
+  require("kickstart.plugins.lazydev"),
+  require("kickstart.plugins.lspconfig"),
+  require("kickstart.plugins.avante"),
+  require("kickstart.plugins.conform"),
+  require("kickstart.plugins.cmp"),
+  require("kickstart.plugins.tokyonight"),
+  require("kickstart.plugins.todo-comments"),
+  require("kickstart.plugins.mini"),
+  require("kickstart.plugins.treesitter"),
+  require("kickstart.plugins.debug"),
+  require("kickstart.plugins.java"),
+}
