baacccad03115a0aa807a23b17eebc66c223a287
Deniz Osmanovic <47736428+DOsmanovic@users.noreply.github.com>
2026-02-18 00:18:45 +0100

    Improve nvim viewing context

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -9,7 +9,7 @@ return {
       statusline.setup { use_icons = vim.g.have_nerd_font }
 
       statusline.section_location = function()
-        return '%2l:%-2v'
+        return '%2l/%L:%-2v %3p%%'
       end
     end,
   },
